c1e1cc1eb2f492586dcd4f32c378fe7821ae3603
Bailey Cosier <bailey.cosier@gmail.com>
2017-09-08 02:03:54 +0700

    Streamlined main cli interface

--- src/main/main.c
+++ src/main/main.c
@@ -84,35 +84,28 @@ int main(int argc, char** argv) {
 
     char* target = NULL;
     char* source = NULL;
-    char* send_note = NULL;
     char* mapsrc = NULL;
+    char* cmds = NULL;
 
     dm_mapping* mapping = NULL;
 
-    int note_ch = 0;
-    int note_vel = 0;
     int keys = 49;
-
-    char* note_on_str;
-    bool note_on = false;
 
     // Specifying the expected options
     // The two options l and b expect numbers as argument
     static struct option long_options[] = {
-        {"list", no_argument, 0, 'l'},
+        {"list-midi", no_argument, 0, 'l'},
         {"help", no_argument, 0, 'h'},
         {"version", no_argument, 0, 'v'},
         {"debug", no_argument, 0, 'z'},
-        {"monitor", no_argument, 0, 'm'},
+        {"mapping", no_argument, 0, 'm'},
         {"keys", required_argument, 0, 'k'},
-        {"remap", required_argument, 0, 'r'},
-        {"send", required_argument, 0, 's'},
         {"target", required_argument, 0, 't'},
         {"source", required_argument, 0, 'x'},
         {0, 0, 0, 0}};
 
     int long_index = 0;
-    while ((opt = getopt_long(argc, argv, "lhvzmr:t:k:x:s:", long_options,
+    while ((opt = getopt_long(argc, argv, "lhvzmr:t:k:x:", long_options,
                               &long_index)) != -1) {
 
         switch (opt) {
@@ -128,10 +121,6 @@ int main(int argc, char** argv) {
             help = true;
             break;
 
-        case 'm':
-            monitor = true;
-            break;
-
         case 'z':
             debug = true;
             break;
@@ -144,39 +133,11 @@ int main(int argc, char** argv) {
             target = optarg;
             break;
 
-        case 's': {
-            int provided = argc - optind;
-            bool invalid_send_args = false;
-
-            if (provided < 3) {
-                handle_invalid_send_args(argc, optind);
-            }
-
-            note_ch = atoi(argv[optind]);
-            note_vel = atoi(argv[optind + 1]);
-            note_on_str = argv[optind + 2];
-
-            if (strcmp(note_on_str, "on") == 0) {
-                note_on = true;
-            } else if (strcmp(note_on_str, "off") == 0) {
-                note_on = false;
-            } else {
-                fprintf(stderr, "Invalid note on/off paramater.\nAccepted "
-                                "options are: 'on' or 'off'. Found %s (%d)\n\n",
-                        note_on_str, optind);
-                handle_invalid_send_args(argc, optind);
-            }
-
-            send_note = optarg;
-
-            break;
-        }
-
         case 'x':
             source = optarg;
             break;
 
-        case 'r':
+        case 'm':
             mapsrc = optarg;
             break;
 
@@ -188,8 +149,8 @@ int main(int argc, char** argv) {
 
     // Capture n amount of mappings provided to the CLI
     if (optind < argc) {
-        char* mapbuf = calloc(sizeof(char*) * 1024, sizeof(char*));
-        char* cat_ptr = mapbuf;
+        cmds = calloc(sizeof(char*) * 1024, sizeof(char*));
+        char* cat_ptr = cmds;
 
         int opts = argc - optind + 1;
         int base = optind;
@@ -204,7 +165,6 @@ int main(int argc, char** argv) {
                 dr_cat(&cat_ptr, ", ");
             }
         }
-        mapsrc = mapbuf;
     }
 
     // printf("mapsrc: %s\noptind: %d/%d\n", mapsrc, optind, argc);
@@ -253,20 +213,6 @@ int main(int argc, char** argv) {
         }
     }
 
-    // Send a midi note to a specific client.
-    if (send_note != NULL) {
-        dr_debug("main: attempting to send midi note");
-        if (target == NULL) {
-            requires_target_specified("send");
-        }
-
-        dm_device* cp = dm_parse_device(target);
-        dr_debug("main: sending midi note to: %s", target);
-        dm_send_midi_to_client(cp->client, cp->port, send_note, note_on,
-                               note_ch, note_vel);
-        return 0;
-    }
-
     // If mappings are provided, check for a valid target and source.
     // Build a MapDefinition struct from cli remap definition.
     if (mapsrc != NULL) {
@@ -290,7 +236,7 @@ int main(int argc, char** argv) {
 
     if (mapping == NULL) {
         dr_error("Invalid Mapping provided\n");
-        printf("-----------------------------------\n");
+        dr_error("-----------------------------------\n");
         print_usage();
         exit(EXIT_FAILURE);
     } else {
