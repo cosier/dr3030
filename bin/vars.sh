df4089037e03144d098baac1fcbd942cd1ef0f1e
Bailey Cosier <bailey.cosier@gmail.com>
2017-09-09 21:51:05 +0700

    Fix exe name

--- bin/vars.sh
+++ bin/vars.sh
@@ -8,11 +8,11 @@ BUILD_DIR=$ROOT/build
 # Apply some cat sed grep trickery to extract
 # APP_LIB_NAME and APP_EXE_NAME from CMake config.
 APP_LIB_NAME=$(cat $ROOT/CMakeLists.txt \
-  | grep -m1 APP_LIB_NAME | sed 's/[a-z].*\s(APP_LIB_NAME\s\(.*\))/\1/')
+  | grep -m1 APP_LIB_NAME | sed 's/[a-z\s(].*APP_LIB_NAME\s\(.*\))/\1/')
 
-APP_MAIN_EXE=$(cat $ROOT/CMakeLists.txt \
-  | grep -m1 APP_LIB_NAME | sed 's/[a-z].*\s(APP_EXE_NAME\s\(.*\))/\1/')
+APP_EXE_NAME=$(cat $ROOT/CMakeLists.txt \
+  | grep -m1 APP_EXE_NAME | sed 's/[a-z\s(].*\APP_EXE_NAME\s\(.*\))/\1/')
 
-EXE_NAME=$APP_MAIN_EXE
+EXE_NAME=$APP_EXE_NAME
 EXE_BUILD=$BUILD_DIR/src/main/$EXE_NAME
 
